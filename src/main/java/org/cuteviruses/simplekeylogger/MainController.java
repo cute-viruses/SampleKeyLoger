748ec572d800d23e33e9443ef85deab53c454552
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-05-20 11:18:48 +0200

    Make a pakeg

--- src/main/java/org/cuteviruses/simplekeylogger/MainController.java
+++ src/main/java/org/cuteviruses/simplekeylogger/MainController.java
@@ -1,3 +1,5 @@
+package org.cuteviruses.simplekeylogger;
+
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
